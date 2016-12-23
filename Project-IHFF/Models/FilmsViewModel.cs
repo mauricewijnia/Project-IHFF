1c0475d53d18d99682398f60afdfc1ec6095eda7
Maurice Wijnia <mauricewijnia@live.nl>
2016-12-23 14:02:03 +0100

    Fix 1241552

--- Project-IHFF/Models/FilmsViewModel.cs
+++ Project-IHFF/Models/FilmsViewModel.cs
@@ -10,6 +10,14 @@ namespace Project_IHFF.Models
         public Films film;
         public FilmExhibitions exhibitionA;
         public FilmExhibitions exhibitionB;
+
+        public FilmsViewModel()
+        {
+            film = new Films();
+
+            exhibitionA = new FilmExhibitions();
+            exhibitionA.startTime = new DateTime(2016, 1, 1, 0, 0, 0);
+        }
         
     }
 }
