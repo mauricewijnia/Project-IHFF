ae98b8ad6567fd18d57584d78cb24528154e86d5
SARPfood <sarpfood@gmail.com>
2017-03-19 15:18:14 +0100

    filmpagina bijna klaar. Sessions + style to go

--- Project-IHFF/Controllers/FilmController.cs
+++ Project-IHFF/Controllers/FilmController.cs
@@ -17,120 +17,50 @@ namespace Project_IHFF.Controllers
         // GET: Film
         public ActionResult Index()
         {
-            /*
-            List<Films> films = filmRepository.GetAllFilmsToList();
-            List<Exhibitions> exhibitions = filmRepository.GetAllExhibitions();
-            List<FilmsViewModel> items = new List<FilmsViewModel>();
-            foreach (Films film in films)
-            {
-                var viewModel = new FilmsViewModel();
-                viewModel.film = film;
-                viewModel.exhibitions = new List<Exhibitions>();
-                viewModel.exhibitions = exhibitions.FindAll(x => x.filmId == film.id);
-                items.Add(viewModel);
-            }*/
 
             IEnumerable<ExhibitionViewModel> allFilms = filmRepository.GetAllFilms();
 
-            
-            //IEnumerable<FilmViewModel> allItems = filmRepository.GetAllFilms();
 
             return View(allFilms);
-
-
-            /*
-            // fake shit--------------------------------------------------------------------------------
-
-            List<Tickets> tickets = new List<Tickets>();
-
-            //fake Film
-            Films film = new Films();
-            film.name = "2Girls1Cup";
-            film.price = 7;
-
-            //Fake Exobision
-            Exhibitions exo = new Exhibitions();
-            exo.endTime = new DateTime(2016, 3, 9, 17, 5, 7, 123);
-            exo.startTime = new DateTime(2016, 3, 9, 16, 5, 7, 123);
-            exo.filmId = 6;
-            exo.Films = film;
-
-
-            //fake FilmTicket;
-            FilmTickets FilmtTicket = new FilmTickets();
-            FilmtTicket.id = 6;
-            FilmtTicket.orderId = 4;
-            FilmtTicket.quantity = 2;
-            FilmtTicket.FilmExhibitions = exo;
-            tickets.Add(FilmtTicket);
-
-            // -------------------------------------------------- 2e ticket
-            //fake Film
-            Films film2 = new Films();
-            film2.name = "SexInTheSicty";
-            film2.price = 2;
-            //Fake Exobision
-            Exhibitions exo2 = new Exhibitions();
-            exo2.endTime = new DateTime(2016, 3, 9, 17, 3, 7, 123);
-            exo2.startTime = new DateTime(2016, 3, 9, 16, 3, 2, 123);
-            exo2.filmId = 5;
-            exo2.Films = film2;
-
-            
-            //fake FilmTicket;
-            FilmTickets FilmtTicket2 = new FilmTickets();
-            FilmtTicket2.id = 5;
-            FilmtTicket2.orderId = 4;
-            FilmtTicket2.quantity = 9;
-            FilmtTicket2.FilmExhibitions = exo2;
-            tickets.Add(FilmtTicket2);
-
-            // -------------------------------------------------- 3e ticket
-            Restaurants restauranttje = new Restaurants();
-            restauranttje.name = "McDonalds";
-            restauranttje.id = 12;
-
-            RestaurantReservation restaurant = new RestaurantReservation();
-            restaurant.id = 22;
-            restaurant.reservationTime = new DateTime(2016, 3, 9, 20, 1, 3, 123);
-            restaurant.quantity = 3;
-            restaurant.Restaurants = restauranttje;
-            tickets.Add(restaurant);
-
-            // -------------------------------------------------- 4e ticket
-            Specials specialtje = new Specials();
-            specialtje.name = "Prater";
-            specialtje.startTime = new DateTime(2016, 3, 8, 13, 2, 3, 123);
-            specialtje.endTime = new DateTime(2016, 3, 8, 15, 2, 3, 123);
-
-            SpecialTicket special = new SpecialTicket();
-            special.id = 23;
-            special.quantity = 3;
-            special.Specials = specialtje;
-            tickets.Add(special);
-            //--------------------------------------------------------------- STOP FAKE
-
-            Session["Tickets"] = tickets; //gooi de lijst met alle tickets in deze Session pls :)
-            return View(tickets);*/
         }
 
-        public virtual ActionResult FilmsView(string day)
+        public virtual ActionResult FilmsView(string day, string place, string sort)
         {
+            //a code to check wich filters are active, and wich query to run
+            string filterCode = (day != null) ? "1" : "0";
+            filterCode = (place != null) ? filterCode + "1" : filterCode + "0";
+            filterCode = (sort == "sortName") ? filterCode + "1" : filterCode + "0";
+
             int dayNr = 0;
-            switch (day)
-            {
-                //dayNr is +1 
-                //query comparision with datetime adds one day to datetime when turned in day
-                case "monday": dayNr = 2; break;
-                case "tuesday": dayNr = 3; break;
-                case "wednesday": dayNr = 4; break;
-                case "thursday": dayNr = 5; break;
-                case "friday": dayNr = 6; break;
-                case "saturday": dayNr = 7; break;
-                case "sunday": dayNr = 8; break;
+            if (day != null) {
+                switch (day)
+                {
+                    //dayNr is +1 
+                    //query comparision with datetime adds one day to datetime when turned in day
+                    case "monday": dayNr = 2; break;
+                    case "tuesday": dayNr = 3; break;
+                    case "wednesday": dayNr = 4; break;
+                    case "thursday": dayNr = 5; break;
+                    case "friday": dayNr = 6; break;
+                    case "saturday": dayNr = 7; break;
+                    case "sunday": dayNr = 8; break;
+                }
             }
-            //if dayNr = 0 (no day filter is set) get all films. otherwise a dayNr is set above and "GetFilmsByDay" is triggerd
-            IEnumerable<ExhibitionViewModel> filmList = (dayNr == 0) ? filmRepository.GetAllFilms() : filmRepository.GetFilmsByDay(dayNr);
+
+            IEnumerable<ExhibitionViewModel> filmList;
+            //fill filmlist with correct filters applied
+            switch (filterCode)
+            {
+                case "100": filmList = filmRepository.GetFilmsByDay(dayNr); break;
+                case "010": filmList = filmRepository.GetFilmsByPlace(place); break;
+                case "110": filmList = filmRepository.GetFilmsByDayPlace(dayNr, place); break;
+                case "001": filmList = filmRepository.GetAllFilmsByTitle(); break;
+                case "101": filmList = filmRepository.GetFilmsByDayTitle(dayNr); break;
+                case "011": filmList = filmRepository.GetFilmsByPlaceTitle(place); break;
+                case "111": filmList = filmRepository.GetFilmsByDayPlaceTitle(dayNr, place); break;
+                default: filmList = filmRepository.GetAllFilms(); break;
+            }
+
             return PartialView(filmList);
         }
 
@@ -139,7 +69,7 @@ namespace Project_IHFF.Controllers
 
             return PartialView();
 
-            //db.Racers.Where(r => r.Country == id).OrderByDescending(r => r.Wins).ThenBy(r => r.Lastname).ToList()
+            
         }
     }
 }
--- Project-IHFF/Repositories/IFilmRepository.cs
+++ Project-IHFF/Repositories/IFilmRepository.cs
@@ -11,6 +11,12 @@ namespace Project_IHFF.Repositories
     {
         IEnumerable<ExhibitionViewModel> GetAllFilms();
         IEnumerable<ExhibitionViewModel> GetFilmsByDay(int day);
+        IEnumerable<ExhibitionViewModel> GetFilmsByPlace(string place);
+        IEnumerable<ExhibitionViewModel> GetFilmsByDayPlace(int day, string place);
+        IEnumerable<ExhibitionViewModel> GetAllFilmsByTitle();
+        IEnumerable<ExhibitionViewModel> GetFilmsByDayTitle(int day);
+        IEnumerable<ExhibitionViewModel> GetFilmsByPlaceTitle(string place);
+        IEnumerable<ExhibitionViewModel> GetFilmsByDayPlaceTitle(int day, string place);
         List<Films> GetAllFilmsToList();
         List<Exhibitions> GetAllExhibitions();
     }
@@ -69,6 +75,166 @@ namespace Project_IHFF.Repositories
                          }).ToList();
             return query;
         }
+        public IEnumerable<ExhibitionViewModel> GetFilmsByPlace(string place)
+        {
+            var query = (from films in ctx.Items.OfType<Films>()
+                         join exhibitions in ctx.Exhibitions on films.id equals exhibitions.filmId
+                         join items in ctx.Items on films.id equals items.id
+                         where items.location.StartsWith(place)
+                         orderby exhibitions.startTime ascending
+
+                         select new ExhibitionViewModel()
+                         {
+                             FilmId = films.id,
+                             ExhibitionId = exhibitions.id,
+                             Name = items.name,
+                             Description = items.description,
+                             Director = films.director,
+                             Actors = films.actors,
+                             Image = items.imagePath,
+                             Location = items.location,
+                             Price = items.price,
+                             Capacity = films.capacity,
+                             StartTime = exhibitions.startTime,
+                             EndTime = exhibitions.endTime
+
+                         }).ToList();
+            return query;
+        }
+
+        public IEnumerable<ExhibitionViewModel> GetFilmsByDayPlace(int day, string place)
+        {
+            var query = (from films in ctx.Items.OfType<Films>()
+                         join exhibitions in ctx.Exhibitions on films.id equals exhibitions.filmId
+                         join items in ctx.Items on films.id equals items.id
+                         where SqlFunctions.DatePart("dw", exhibitions.startTime) == day
+                         where items.location.StartsWith(place)
+                         orderby exhibitions.startTime ascending
+
+                         select new ExhibitionViewModel()
+                         {
+                             FilmId = films.id,
+                             ExhibitionId = exhibitions.id,
+                             Name = items.name,
+                             Description = items.description,
+                             Director = films.director,
+                             Actors = films.actors,
+                             Image = items.imagePath,
+                             Location = items.location,
+                             Price = items.price,
+                             Capacity = films.capacity,
+                             StartTime = exhibitions.startTime,
+                             EndTime = exhibitions.endTime
+
+                         }).ToList();
+            return query;
+        }
+
+        public IEnumerable<ExhibitionViewModel> GetAllFilmsByTitle()
+        {
+            var query = (from films in ctx.Items.OfType<Films>()
+                         join exhibitions in ctx.Exhibitions on films.id equals exhibitions.filmId
+                         join items in ctx.Items on films.id equals items.id
+                         orderby items.name ascending
+                         select new ExhibitionViewModel()
+                         {
+                             FilmId = films.id,
+                             ExhibitionId = exhibitions.id,
+                             Name = items.name,
+                             Description = items.description,
+                             Director = films.director,
+                             Actors = films.actors,
+                             Image = items.imagePath,
+                             Location = items.location,
+                             Price = items.price,
+                             Capacity = films.capacity,
+                             StartTime = exhibitions.startTime,
+                             EndTime = exhibitions.endTime
+                         }).ToList();
+            return query;
+        }
+
+        public IEnumerable<ExhibitionViewModel> GetFilmsByDayTitle(int day)
+        {
+            var query = (from films in ctx.Items.OfType<Films>()
+                         join exhibitions in ctx.Exhibitions on films.id equals exhibitions.filmId
+                         join items in ctx.Items on films.id equals items.id
+                         where SqlFunctions.DatePart("dw", exhibitions.startTime) == day
+                         orderby items.name ascending
+
+                         select new ExhibitionViewModel()
+                         {
+                             FilmId = films.id,
+                             ExhibitionId = exhibitions.id,
+                             Name = items.name,
+                             Description = items.description,
+                             Director = films.director,
+                             Actors = films.actors,
+                             Image = items.imagePath,
+                             Location = items.location,
+                             Price = items.price,
+                             Capacity = films.capacity,
+                             StartTime = exhibitions.startTime,
+                             EndTime = exhibitions.endTime
+
+                         }).ToList();
+            return query;
+        }
+        public IEnumerable<ExhibitionViewModel> GetFilmsByPlaceTitle(string place)
+        {
+            var query = (from films in ctx.Items.OfType<Films>()
+                         join exhibitions in ctx.Exhibitions on films.id equals exhibitions.filmId
+                         join items in ctx.Items on films.id equals items.id
+                         where items.location.StartsWith(place)
+                         orderby items.name ascending
+
+                         select new ExhibitionViewModel()
+                         {
+                             FilmId = films.id,
+                             ExhibitionId = exhibitions.id,
+                             Name = items.name,
+                             Description = items.description,
+                             Director = films.director,
+                             Actors = films.actors,
+                             Image = items.imagePath,
+                             Location = items.location,
+                             Price = items.price,
+                             Capacity = films.capacity,
+                             StartTime = exhibitions.startTime,
+                             EndTime = exhibitions.endTime
+
+                         }).ToList();
+            return query;
+        }
+
+        public IEnumerable<ExhibitionViewModel> GetFilmsByDayPlaceTitle(int day, string place)
+        {
+            var query = (from films in ctx.Items.OfType<Films>()
+                         join exhibitions in ctx.Exhibitions on films.id equals exhibitions.filmId
+                         join items in ctx.Items on films.id equals items.id
+                         where SqlFunctions.DatePart("dw", exhibitions.startTime) == day
+                         where items.location.StartsWith(place)
+                         orderby items.name ascending
+
+                         select new ExhibitionViewModel()
+                         {
+                             FilmId = films.id,
+                             ExhibitionId = exhibitions.id,
+                             Name = items.name,
+                             Description = items.description,
+                             Director = films.director,
+                             Actors = films.actors,
+                             Image = items.imagePath,
+                             Location = items.location,
+                             Price = items.price,
+                             Capacity = films.capacity,
+                             StartTime = exhibitions.startTime,
+                             EndTime = exhibitions.endTime
+
+                         }).ToList();
+            return query;
+        }
+
 
         public List<Exhibitions> GetAllExhibitions()
         {
