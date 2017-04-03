5ba595ca5f41f5a946dac7a5b2bc36f552b77d2c
SARPfood <sarpfood@gmail.com>
2017-04-04 00:47:00 +0200

    placed some js from view to js file(working)

--- Project-IHFF/Scripts/custom.js
+++ Project-IHFF/Scripts/custom.js
@@ -1,1 +1,26 @@
 ﻿
+//loads the partial view with a list of items for special and film page (based on active filters)
+$(document).ready(function ()
+{
+    $(".sortBtn").click(function ()
+    {
+        //all (old) selected filters
+        var day = $('input[name=filterDay]:checked').attr('id');
+        var place = $('input[name=filterPlace]:checked').attr('id');
+        var order = $('input[name=filterOrder]:checked').attr('id');
+
+        //replace new selected with old selected
+        if ($(this).hasClass("sortDay")) { day = $(this).attr('id') }
+        if ($(this).hasClass("sortPlace")) { place = $(this).attr('id') }
+        if ($(this).hasClass("sortOrder")) { order = $(this).attr('id') }
+
+        // get the @url.actionresult from the page
+        var url = $("#itemsContainer").data('request-url');
+
+        //use action result and pass parameters, load it in the container div
+        $("#itemsContainer").load(url, { 'day': day, 'place': place, 'sort': order });
+    });
+});
+
+
+
